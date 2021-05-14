b4a11f0b2bbc81c67fee9cdb0c87b43d0adc72fb
dsurnin <dsurnin@ozon.ru>
2021-05-14 14:34:11 +0300

    null string fix

--- hive_generator/lib/src/class_builder.dart
+++ hive_generator/lib/src/class_builder.dart
@@ -72,7 +72,7 @@ class ClassBuilder extends Builder {
   String _value(DartType type, String variable, DartObject? defaultValue) {
     var value = _cast(type, variable);
     if (defaultValue?.isNull != false) return value;
-    return '$variable == null ? "null" : $value';
+    return '$variable == null ? null : $value';
   }
 
   String _cast(DartType type, String variable) {
